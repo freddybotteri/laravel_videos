59ce04fbe4ef130204308a4e70b51763ba32a565
FREDDY MILLA <freddybotteri@gmail.com>
2022-09-10 20:15:29 +0200

    vite project

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $users = App\Models\User::get();
+
+
+    return view('welcome', ['users' => $users]);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/patient', [App\Http\Controllers\PatientController::class, 'index'])->name('patient');
+
